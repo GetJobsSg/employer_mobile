46fc3bc3f97e10e648a4768b9f9036e37f71b7a7
BENSTOH <benson.toh@daimler.com>
2021-05-17 21:48:08 +0800

    initial commit with code quality tools

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>HELLO WORLD</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
