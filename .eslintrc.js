aa18a2135f315e5b6fae07fd1298704a7b4a6f0f
benson toh <benson_7667@hotmail.com>
2021-07-01 21:56:58 +0800

    chore: install typescript eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,26 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'airbnb-typescript', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  parserOptions: {
+    createDefaultProgram: true,
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: { es6: true },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'global-require': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-param-reassign': 'off',
+    'react/no-array-index-key': 'warn',
+    'react/require-default-props': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App = () => (
+  <SafeAreaView>
+    <View>
+      <Text>HELLO WORLD</Text>
+    </View>
+  </SafeAreaView>
+);
+
+export default App;
