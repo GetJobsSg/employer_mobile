cc2f42b3ec648ef0b234a2fcf286467260f6a162
benson toh <benson_7667@hotmail.com>
2021-10-06 22:34:40 +0800

    feat: released for android internal testing

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const config = {
   networkTimeout: 10000,
-  apiHost: 'http://192.168.1.10:8080/',
+  apiHost: 'http://getjobs-dev-backend.eba-iukcicq5.ap-southeast-1.elasticbeanstalk.com/',
   stripePublishableKey:
     'pk_test_51ItwBSI0rX7YUOukv2SDEsDqavwKDz9TxT1ZpNmK7hTJPld4nD7a9N84rg5LMZR08DBgIa4RyMIZUFybQaYUPGzn00P9HMKRM8',
   // apiHost: 'http://localhost:8080/',
